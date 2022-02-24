2fa442ad631b76ed1b9c33bc01112e57417651fc
602115 <602115@stud.hvl.no>
2022-02-24 19:17:30 +0100

    Lagde index.java og Main metode

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean antall = true;
+        if (antall) {
+            System.out.println(antall);
+        }
+    }
+}
